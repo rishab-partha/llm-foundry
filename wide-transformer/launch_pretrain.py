fc8c92f7dada0d149f6534b2b84e4004f039ea54
Zack Ankner <zackankner@gmail.com>
2023-06-26 15:01:02 -0700

    Add launch scripts and benchmark configuration for wide-transformer

--- wide-transformer/launch_pretrain.py
+++ wide-transformer/launch_pretrain.py
@@ -8,6 +8,7 @@ if __name__ == "__main__":
     # System args
     parser.add_argument("--cluster", type=str, default="r7z2")
     parser.add_argument("--ngpus", type=int, default=32)
+    parser.add_argument("--device-batch-size", type=int, default=None)
     parser.add_argument("--autoresume", action="store_true")
     parser.add_argument("--preemptible", action="store_true")
     parser.add_argument("--seeds", nargs="+", type=int,
@@ -15,6 +16,7 @@ if __name__ == "__main__":
     parser.add_argument("--local-debug", action="store_true")
 
     # Model args
+    parser.add_argument("--lrs", nargs="+", type=float, default=[2e-4, 1e-4, 5e-5, 2.5e-5])
     parser.add_argument("--d-model", type=int, required=True)
     parser.add_argument("--n-layers", type=int, required=True)
     parser.add_argument("--expansion-ratio", type=int, required=True)
@@ -24,50 +26,56 @@ if __name__ == "__main__":
     run_base = f"1B-dmodel-{args.d_model}-layers-{args.n_layers}-expansion-{args.expansion_ratio}"
 
     for seed in args.seeds:
-        base_run = RunConfig.from_file(
-            f"wide-transformer/yamls/pretrain/base.yaml")
+        for lr in args.lrs:
+            base_run = RunConfig.from_file(
+                f"wide-transformer/yamls/pretrain/base.yaml")
 
-        run_name = f"{run_base}-sd-{seed}"
-        base_run.name = run_name.lower()
-        base_run.parameters["run_name"] = run_name
+            run_name = f"{run_base}-{lr}-sd-{seed}"
+            base_run.name = run_name.lower()
+            base_run.parameters["run_name"] = run_name
 
-        base_run.parameters["model"]["d_model"] = args.d_model
-        base_run.parameters["model"]["n_layers"] = args.n_layers
-        base_run.parameters["model"]["expansion_ratio"] = args.expansion_ratio
+            base_run.parameters["model"]["d_model"] = args.d_model
+            base_run.parameters["model"]["n_layers"] = args.n_layers
+            base_run.parameters["model"]["expansion_ratio"] = args.expansion_ratio
 
-        base_run.cluster = args.cluster
-        base_run.gpu_num = args.ngpus
+            base_run.parameters["optimizer"]["lr"] = lr
+            base_run.parameters["optimizer"]["weight_decay"] = lr # Using lion so lr / wd are the same
 
-        if args.preemptible:
-            base_run.scheduling = {"resumable": True, "priority": "low"}
-            base_run.parameters["autoresume"] = True
-        else:
-            base_run.parameters["autoresume"] = args.autoresume
+            base_run.cluster = args.cluster
+            base_run.gpu_num = args.ngpus
 
-        base_run.parameters["global_seed"] = seed
+            if args.preemptible:
+                base_run.scheduling = {"resumable": True, "priority": "low"}
+                base_run.parameters["autoresume"] = True
+            else:
+                base_run.parameters["autoresume"] = args.autoresume
 
-        base_run.parameters["loggers"]["wandb"]["tags"] = [
-            "1B", "pile", f"dmodel-{args.d_model}", f"layers-{args.n_layers}",
-            f"expansion-{args.expansion_ratio}"
-        ]
+            base_run.parameters["global_seed"] = seed
 
-        if args.cluster in ["r1z1", "r8z6"]:
-            gpu_type = "a100_80gb"
-            device_batch_size = 16
-        else:
-            gpu_type = "a100_40gb"
-            device_batch_size = 4
+            base_run.parameters["loggers"]["wandb"]["tags"] = [
+                "1B", "pile", f"dmodel-{args.d_model}", f"layers-{args.n_layers}",
+                f"expansion-{args.expansion_ratio}", f"lr-{lr}"
+            ]
 
-        base_run.gpu_type = gpu_type
-        base_run.parameters["device_train_microbatch_size"] = device_batch_size
-        base_run.parameters["device_eval_batch_size"] = device_batch_size
+            if args.cluster in ["r1z1", "r8z6"]:
+                gpu_type = "a100_80gb"
+                device_batch_size = 8
+            else:
+                gpu_type = "a100_40gb"
+                device_batch_size = 4
+            if args.device_batch_size is not None:
+                device_batch_size = args.device_batch_size
 
-        if args.local_debug:
-            with open("debug.yaml", "w") as f:
-                OmegaConf.save(config=OmegaConf.create(base_run.parameters),
-                               f=f)
-        else:
-            run = create_run(base_run)
-            print(
-                f"Launched reference training for seed {seed} with in {run.name}"
-            )
+            base_run.gpu_type = gpu_type
+            base_run.parameters["device_train_microbatch_size"] = device_batch_size
+            base_run.parameters["device_eval_batch_size"] = device_batch_size
+
+            if args.local_debug:
+                with open("debug.yaml", "w") as f:
+                    OmegaConf.save(config=OmegaConf.create(base_run.parameters),
+                                   f=f)
+            else:
+                run = create_run(base_run)
+                print(
+                    f"Launched reference training for seed {seed} with in {run.name}"
+                )
