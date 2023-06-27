978ed5043afb64c00fae67232bef70735a4b889d
Zack Ankner <zackankner@gmail.com>
2023-06-27 10:12:11 -0700

    Add 4e-4 and 2e-4 lrs, set max_duration with batches

--- wide-transformer/launch_pretrain.py
+++ wide-transformer/launch_pretrain.py
@@ -16,7 +16,8 @@ if __name__ == "__main__":
     parser.add_argument("--local-debug", action="store_true")
 
     # Model args
-    parser.add_argument("--lrs", nargs="+", type=float, default=[2e-4, 1e-4, 5e-5, 2.5e-5])
+    parser.add_argument("--lrs", nargs="+", type=float, default=[4e-4, 2e-4])
+    parser.add_argument("--batches", type=int ,required=True)
     parser.add_argument("--d-model", type=int, required=True)
     parser.add_argument("--n-layers", type=int, required=True)
     parser.add_argument("--expansion-ratio", type=int, required=True)
@@ -30,8 +31,8 @@ if __name__ == "__main__":
             base_run = RunConfig.from_file(
                 f"wide-transformer/yamls/pretrain/base.yaml")
 
-            run_name = f"{run_base}-{lr}-sd-{seed}"
-            base_run.name = run_name.lower()
+            base_run.name = run_base.lower().replace(".", "-")
+            run_name = f"{run_base}-{lr:.6f}-ba-{args.batches}-sd-{seed}"
             base_run.parameters["run_name"] = run_name
 
             base_run.parameters["model"]["d_model"] = args.d_model
@@ -40,6 +41,8 @@ if __name__ == "__main__":
 
             base_run.parameters["optimizer"]["lr"] = lr
             base_run.parameters["optimizer"]["weight_decay"] = lr # Using lion so lr / wd are the same
+
+            base_run.parameters["max_duration"] = f"{args.batches}ba"
 
             base_run.cluster = args.cluster
             base_run.gpu_num = args.ngpus
@@ -60,6 +63,10 @@ if __name__ == "__main__":
             if args.cluster in ["r1z1", "r8z6"]:
                 gpu_type = "a100_80gb"
                 device_batch_size = 8
+            elif args.cluster in ["r9z1"]:
+                gpu_type = "h100_80gb"
+                device_batch_size = 8
+                base_run.image = "mosaicml/llm-foundry:2.0.1_cu118-latest"
             else:
                 gpu_type = "a100_40gb"
                 device_batch_size = 4
